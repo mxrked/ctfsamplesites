c17542cd65ae4b3dd19dabb4e8dbcda385b71298
Parker <pp101socials@gmail.com>
2023-10-12 08:41:17 -0400

    Added useEffects

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 // React/Next Imports
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 
 // Library Imports
@@ -13,6 +13,10 @@ import "lazysizes/plugins/parent-fit/ls.parent-fit";
 import NProgress from "nprogress";
 
 // Data/Functions/Images Imports
+import DeclareStorageVariable from "@/assets/functions/data/storage/DeclareStorageVariable";
+import RemoveStorageVariable from "@/assets/functions/data/storage/RemoveStorageVariable";
+import CheckUserDevice from "@/assets/functions/dom/checkers/CheckUserDevice";
+import CheckScreenOrientation from "@/assets/functions/dom/checkers/CheckScreenOrientation";
 
 // Component Imports
 
@@ -24,22 +28,9 @@ import "../assets/styles/tools/library_styles/nprogress/nprogress.css";
 
 function MyApp({ Component, pageProps }) {
   const router = useRouter();
+  const [updateUI, setUpdateUI] = useState(0);
 
-  //! Session/Local Storage Clearing
-  useEffect(() => {
-    if (localStorage.getItem("ally-supports-cache")) {
-      localStorage.removeItem("ally-supports-cache");
-    }
-
-    if (sessionStorage.getItem("Search Opened")) {
-      sessionStorage.removeItem("Search Opened");
-    }
-
-    if (sessionStorage.getItem("Mobile Nav Opened")) {
-      sessionStorage.removeItem("Mobile Nav Opened");
-    }
-  }, [router]);
-
+  //? GLOBALS
   //! NProgress Init
   useEffect(() => {
     // NProgress.done(); // Prevents NProgress from being stuck after page route completed
@@ -53,7 +44,237 @@ function MyApp({ Component, pageProps }) {
     });
   }, [router, router.events]);
 
-  return <Component {...pageProps} />;
+  //! Forget scroll position and force user back to top of page
+  useEffect(() => {
+    if ("scrollRestoration" in history) {
+      history.scrollRestoration = "manual";
+    }
+
+    window.addEventListener("beforeunload", () => {
+      if (window.scrollY !== 0) {
+        DeclareStorageVariable("session", "Reload Scroll", true);
+      }
+    });
+
+    if (sessionStorage.getItem("Reload Scroll")) {
+      window.scrollTo({ top: 0, left: 0, behavior: "instant" });
+
+      RemoveStorageVariable("session", "Reload Scroll");
+    }
+  }, []);
+
+  //! Updating UI State
+  useEffect(() => {
+    setTimeout(() => {
+      if (sessionStorage.getItem("FM Loaded")) {
+        setUpdateUI(updateUI + 1);
+      }
+    }, 1700);
+  }, [router]);
+
+  //! Enabling scrolling and pointerevents when updateUI == 1
+  useEffect(() => {
+    setTimeout(() => {
+      if (updateUI == 1) {
+        document.body.style.overflowY = "auto";
+        document.body.style.pointerEvents = "auto";
+      }
+    }, 500);
+  }, [updateUI]);
+
+  //! Reload Page after route change (This is mostly for the animations)
+  useEffect(() => {
+    router.events.on("routeChangeComplete", () => {
+      router.reload();
+    });
+  }, [router]);
+
+  //? DATA
+  //! Session/Local Storage Clearing
+  useEffect(() => {
+    if (!sessionStorage.getItem("EA Fix")) {
+      DeclareStorageVariable("session", "EA Fix", true);
+      router.reload();
+    }
+
+    if (sessionStorage.getItem("EA Fix")) {
+      const SS_VARIABLES = [
+        "Modal Opened",
+        "FM Loaded",
+        "Page Reload",
+        "Submission Sent",
+        "ph_foZTeM1AW8dh5WkaofxTYiInBhS4XzTzRqLs50kVziw_posthog",
+      ];
+      const LS_VARIABLES = ["ally-supports-cache"];
+
+      SS_VARIABLES.forEach((variable) => {
+        RemoveStorageVariable("session", variable);
+      });
+      LS_VARIABLES.forEach((variable) => {
+        RemoveStorageVariable("local", variable);
+      });
+    }
+  }, [router]);
+
+  //! Adding value after framer motion content has loaded
+  useEffect(() => {
+    setTimeout(() => {
+      DeclareStorageVariable("session", "FM Loaded", true);
+    }, 2000);
+
+    window.addEventListener("beforeunload", () => {
+      RemoveStorageVariable("session", "FM Loaded");
+    });
+  }, [router]);
+
+  //? MANIPS
+  //! Determine to enable/disable framer motion animations on mobile devices
+  useEffect(() => {
+    //TODO: THIS IS USED TO ENABLE/DISABLE THE FRAMER MOTION ANIMATIONS ON MOBILE DEVICES
+    // ----------------------------------------------------
+    const TURN_OFF_FRAMER_MOTION_ON_MOBILE_DEVICES = true;
+    const TURN_OFF_FRAMER_MOTION_ON_DESKTOP = false;
+    // ----------------------------------------------------
+
+    // Checking to see if user is on desktop
+    let onDesktop = true;
+    let onMobile = true;
+    if (sessionStorage.getItem("Mobile Device")) {
+      onDesktop = false;
+      onMobile = true;
+    }
+
+    if (sessionStorage.getItem("Desktop Device")) {
+      onDesktop = true;
+      onMobile = false;
+    }
+
+    const handleDisableCheck = () => {
+      // if (!TURN_OFF_FRAMER_MOTION_ON_MOBILE_DEVICES) {
+      //   return;
+      // } else {
+      //   if (!onDesktop) {
+      //     document.querySelectorAll(".fm-element").forEach((fix) => {
+      //       fix.classList.add("fm-mobile-fix");
+      //     });
+      //   }
+      // }
+
+      //! DESKTOP
+      if (TURN_OFF_FRAMER_MOTION_ON_DESKTOP) {
+        if (onDesktop && !onMobile) {
+          document.querySelectorAll(".fm-element").forEach((fix) => {
+            fix.classList.add("fm-mobile-fix");
+          });
+        }
+      }
+
+      //! MOBILE
+      if (TURN_OFF_FRAMER_MOTION_ON_MOBILE_DEVICES) {
+        if (!onDesktop && onMobile) {
+          document.querySelectorAll(".fm-element").forEach((fix) => {
+            fix.classList.add("fm-mobile-fix");
+          });
+        }
+      }
+
+      //! BOTH
+      if (
+        TURN_OFF_FRAMER_MOTION_ON_DESKTOP &&
+        TURN_OFF_FRAMER_MOTION_ON_MOBILE_DEVICES
+      ) {
+        document.querySelectorAll(".fm-element").forEach((fix) => {
+          fix.classList.add("fm-mobile-fix");
+        });
+      }
+    };
+
+    // Adds the class to disable the animations if DETERMINE_FRAMER_MOTION_ON_MOBILE_DEVICES = true
+    handleDisableCheck();
+  }, []);
+
+  //? CHECKERS
+  //! Check Page Orientation
+  useEffect(() => {
+    window.addEventListener("orientationchange", () => {
+      CheckScreenOrientation();
+    });
+  }, []);
+
+  //! Check User Device
+  useEffect(() => {
+    let mobile,
+      desktop = false;
+
+    // Page Load
+    window.addEventListener("load", () => {
+      setTimeout(() => {
+        CheckUserDevice(mobile, desktop);
+      }, 500);
+    });
+
+    if (document.readyState === "complete") {
+      setTimeout(() => {
+        CheckUserDevice(mobile, desktop);
+      }, 500);
+    }
+
+    // Resize
+    window.addEventListener("resize", () => {
+      CheckUserDevice(mobile, desktop);
+    });
+  }, []);
+
+  //? DISPLAYS/HIDERS
+  //! Add selection styling for specific elements by adding a className to each element
+  useEffect(() => {
+    const ELEMENT_TYPES = [
+      "h1",
+      "h2",
+      "h3",
+      "h4",
+      "h5",
+      "h6",
+      "p",
+      "span",
+      "a",
+      "a span",
+      "li",
+      "img",
+      "br",
+      "strong",
+      "button",
+      "label",
+      "select",
+      "input",
+      "textarea",
+      ':contains("&nbsp;")',
+    ];
+
+    ELEMENT_TYPES.forEach((eT) => {
+      const ELEMENTS = document.getElementsByTagName(eT);
+
+      for (let i = 0; i < ELEMENTS.length; i++) {
+        if (!ELEMENTS[i].classList.contains("selected")) {
+          ELEMENTS[i].classList.add("selected");
+        }
+      }
+    });
+  }, []);
+
+  // useEffect(() => {
+  //   window.addEventListener("scroll", () => {
+  //     if (!sessionStorage.getItem("Modal Opened")) {
+  //       document.body.style.pointerEvents = "auto";
+  //     }
+  //   });
+  // }, []);
+
+  return (
+    <AnimatePresence mode="wait">
+      <Component {...pageProps} />
+    </AnimatePresence>
+  );
 }
 
 export default MyApp;
