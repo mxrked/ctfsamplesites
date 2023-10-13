4a7af364a3d6e0ac86713c9cd546e72a96f8fb8f
Parker <pp101socials@gmail.com>
2023-10-13 08:38:28 -0400

    Created file

--- assets/components/sites/Main/Nav/DesktopNav.js
+++ assets/components/sites/Main/Nav/DesktopNav.js
@@ -4,6 +4,7 @@
  *
  */
 
+import { useState } from "react";
 import { useRouter } from "next/router";
 
 import { FaCaretDown } from "react-icons/fa";
@@ -12,6 +13,9 @@ import styles from "../../../../styles/modules/Nav/Main/Nav.module.css";
 
 export const DesktopNav = (props) => {
   const router = useRouter();
+
+  const [IS_CHECKBOX_CHECKED, SET_IS_CHECKBOX_CHECKED] = useState(false);
+  const [IS_CHECKBOX_HOVERED, SET_IS_CHECKBOX_HOVERED] = useState(false);
 
   return (
     <nav id="desktopNav" className={`${styles.desktop_nav}`}>
@@ -42,30 +46,98 @@ export const DesktopNav = (props) => {
               <div className={`${styles.desktop_nav_inner_side_cnt}`}>
                 <ul className={`${styles.nav_links}`}>
                   <li>
-                    Sample Websites <FaCaretDown className={`${styles.icon}`} />
+                    <span
+                      className={`${styles.link_main} orientation-change-element half-second`}
+                      id="desktopNavLinksLinkMain"
+                    >
+                      Samples{" "}
+                      <FaCaretDown
+                        className={`${styles.icon} icon half-second`}
+                      />
+                    </span>
                     <input
                       type={"checkbox"}
-                      onBlur={(e) => {
-                        const SAMPLE_SITE_LINKS = document.getElementById(
-                          "desktopNavSampleSitesLinks"
-                        );
-
-                        if (e.currentTarget.checked) {
-                          e.currentTarget.checked = false;
-                          setTimeout(() => {
-                            SAMPLE_SITE_LINKS.style.display = "none";
-                          }, 200);
+                      // Handle mouse enter event
+                      onMouseEnter={(e) => {
+                        // Set the checkbox hover state to true
+                        SET_IS_CHECKBOX_HOVERED(true);
+                        // Change the color of the icon to turquoise
+                        document
+                          .getElementById("desktopNavLinksLinkMain")
+                          .querySelector(".icon").style.color = "#3fd7d7";
+                      }}
+                      // Handle mouse leave event
+                      onMouseLeave={(e) => {
+                        // Set the checkbox hover state to false
+                        SET_IS_CHECKBOX_HOVERED(false);
+                        // If the checkbox is not checked, change the color of the icon to dark gray
+                        if (!e.currentTarget.checked) {
+                          document
+                            .getElementById("desktopNavLinksLinkMain")
+                            .querySelector(".icon").style.color = "#202020";
                         }
                       }}
-                      onChange={(e) => {
+                      // Handle blur event
+                      onBlur={(e) => {
+                        // Uncheck the checkbox
+                        e.currentTarget.checked = false;
+                        // Set the checkbox checked state
+                        SET_IS_CHECKBOX_CHECKED(e.currentTarget.checked);
+
+                        // Get the sample sites links element
                         const SAMPLE_SITE_LINKS = document.getElementById(
                           "desktopNavSampleSitesLinks"
                         );
 
+                        // Use a timeout to allow for smoother transitions
+                        setTimeout(() => {
+                          // If not hovered and not checked, change the color of the icon to dark gray
+                          if (!IS_CHECKBOX_HOVERED && !IS_CHECKBOX_CHECKED) {
+                            document
+                              .getElementById("desktopNavLinksLinkMain")
+                              .querySelector(".icon").style.color = "#202020";
+                            // Hide the sample sites links
+                            SAMPLE_SITE_LINKS.style.display = "none";
+                          }
+                          // If hovered, change the color of the icon to turquoise
+                          else if (IS_CHECKBOX_HOVERED) {
+                            document
+                              .getElementById("desktopNavLinksLinkMain")
+                              .querySelector(".icon").style.color = "#3fd7d7";
+                          }
+                        }, 200);
+                      }}
+                      // Handle checkbox change event
+                      onChange={(e) => {
+                        // Get the sample sites links element
+                        const SAMPLE_SITE_LINKS = document.getElementById(
+                          "desktopNavSampleSitesLinks"
+                        );
+
+                        // If the checkbox is checked
                         if (e.currentTarget.checked) {
+                          // Show the sample sites links
                           SAMPLE_SITE_LINKS.style.display = "block";
-                        } else {
+                          // Change the color of the icon to turquoise
+                          document
+                            .getElementById("desktopNavLinksLinkMain")
+                            .querySelector(".icon").style.color = "#3fd7d7";
+                        }
+                        // If the checkbox is unchecked
+                        else {
+                          // Hide the sample sites links
                           SAMPLE_SITE_LINKS.style.display = "none";
+                          // Change the color of the icon to dark gray
+                          document
+                            .getElementById("desktopNavLinksLinkMain")
+                            .querySelector(".icon").style.color = "#202020";
+
+                          // If hovered, change the color of the icon to turquoise
+                          if (IS_CHECKBOX_HOVERED) {
+                            document
+                              .getElementById("desktopNavLinksLinkMain")
+                              .querySelector(".icon").style.color = "#3fd7d7";
+                          }
                         }
                       }}
                     />
