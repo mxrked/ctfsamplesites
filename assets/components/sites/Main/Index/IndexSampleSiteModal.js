7c1e82fca214a6d19e44ee035844450eb82cadc1
Parker <pp101socials@gmail.com>
2023-10-18 10:34:11 -0400

    Created file

---
+++ assets/components/sites/Main/Index/IndexSampleSiteModal.js
@@ -0,0 +1,11 @@
+/**
+ *
+ *  This is the Index Sample Site Modal
+ *
+ */
+
+import styles from "../../../../styles/modules/Sites/Main/Main.module.css";
+
+export const IndexSampleSiteModal = ({ sample_site_modal_data }) => {
+  return "";
+};
